9c5b5230833fa0f3edab5092095920f4cce4a5a6
Matthew R. Becker <becker.mr@gmail.com>
2020-11-21 09:02:32 -0600

    ENH get it rolling

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="conda-forge-artifact-validation",
+    version="0.0.1",
+    description="",
+    author="conda-forge/core",
+    author_email="",
+    scripts=["bin/conda-forge-tick"],
+    url="https://github.com/conda-forge/artifact-validation",
+    packages=find_packages(),
+)
