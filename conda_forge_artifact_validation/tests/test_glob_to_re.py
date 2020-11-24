4bf1ed0db21dcce388b9d6e1a6021e807bb8fc16
Matthew R. Becker <becker.mr@gmail.com>
2020-11-24 07:32:51 -0600

    blacken

--- conda_forge_artifact_validation/tests/test_glob_to_re.py
+++ conda_forge_artifact_validation/tests/test_glob_to_re.py
@@ -9,20 +9,23 @@ import pytest
 from ..glob_to_re import glob_to_re
 
 
-@pytest.mark.parametrize('valid,path,glob_pat', [
-    (True, 'foo.py', 'foo.py'),
-    (True, 'foo.py', 'fo[o].py'),
-    (True, 'fob.py', 'fo[!o].py'),
-    (True, '*foo.py', '[*]foo.py'),
-    (True, 'foo.py', '**/foo.py'),
-    (True, 'baz/duck/bar/bam/quack/foo.py', '**/bar/**/foo.py'),
-    (True, 'bar/foo.py', '**/foo.py'),
-    (True, 'bar/baz/foo.py', 'bar/**'),
-    (False, 'bar/baz/foo.py', 'bar/*'),
-    (False, 'bar/baz/foo.py', 'bar**/foo.py'),
-    (True, 'bar/baz/foo.py', 'bar/**/foo.py'),
-    (True, 'bar/baz/wut/foo.py', 'bar/**/foo.py'),
-])
+@pytest.mark.parametrize(
+    "valid,path,glob_pat",
+    [
+        (True, "foo.py", "foo.py"),
+        (True, "foo.py", "fo[o].py"),
+        (True, "fob.py", "fo[!o].py"),
+        (True, "*foo.py", "[*]foo.py"),
+        (True, "foo.py", "**/foo.py"),
+        (True, "baz/duck/bar/bam/quack/foo.py", "**/bar/**/foo.py"),
+        (True, "bar/foo.py", "**/foo.py"),
+        (True, "bar/baz/foo.py", "bar/**"),
+        (False, "bar/baz/foo.py", "bar/*"),
+        (False, "bar/baz/foo.py", "bar**/foo.py"),
+        (True, "bar/baz/foo.py", "bar/**/foo.py"),
+        (True, "bar/baz/wut/foo.py", "bar/**/foo.py"),
+    ],
+)
 def test_glob_to_re(valid, path, glob_pat):
     re_pat = glob_to_re(glob_pat)
     match = re.fullmatch(re_pat, path)
