63595b49a1a24b59378ec59f6db2da035a15c612
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-12-27 14:38:10 -0600

    Update validate.py

--- conda_forge_artifact_validation/validate.py
+++ conda_forge_artifact_validation/validate.py
@@ -129,7 +129,9 @@ def download_and_validate(
 
     _, pkg = subdir_pkg.split(os.path.sep)
 
-    with tempfile.TemporaryDirectory(dir=os.environ.get("GITHUB_WORKSPACE", None)) as tmpdir:
+    with tempfile.TemporaryDirectory(
+        dir=os.environ.get("GITHUB_WORKSPACE", None)
+    ) as tmpdir:
         try:
             # download
             subprocess.run(
